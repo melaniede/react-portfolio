f306753ac1a18e71f9b9aeee668f525a191f2b8f
melaniede <debarrosmelanie@gmail.com>
2020-11-07 22:03:46 -0500

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./style.css";
+
+function Footer () {
+    return (
+        <footer class="footer bg-secondary">
+        <div>
+            <p class="text-white">© 2020 Copyright</p>
+        </div>
+        </footer>
+    )
+}
+
+export default Footer;
+
+
+// import React from "react";
+// import "./style.css";
+
+// function Header () {
+//     return (
+//         <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+//         <h5 class="my-0 mr-md-auto font-weight-normal">Melanie DeBarros</h5>
+//         <nav class="my-2 my-md-0 mr-md-3">
+//           <a class="p-2 text-dark" href="index.html">About</a>
+//           <a class="p-2 text-dark" href="portfolio.html">Portfolio</a>
+//           <a class="p-2 text-dark" href="contact.html">Contact</a>
+//         </nav>
+//       </div>
+//     )
+// }
+
+// export default Header;
